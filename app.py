4620838898c8e30ed63c8f50e40793cfa7af6494
Anusha Kalbande <anushakalbande17@gmail.com>
2024-07-20 19:01:35 -0400

    Resolving the issue of creating pinecone index repeatedly

--- app.py
+++ app.py
@@ -1,29 +1,53 @@
 import streamlit as st
 from dotenv import find_dotenv, load_dotenv
-import pdf_process as pp
+from pdf_process import *
 
 def load_api_keys():
-    #load_dotenv(".\VSOC\.env.txt")
     load_dotenv(find_dotenv(".env.txt"))
+
+def initialize_session_state():
+    if 'file_uploaded' not in st.session_state:
+        st.session_state.file_uploaded = False
+    if 'pdf_processed' not in st.session_state:
+        st.session_state.pdf_processed = False
+
+def upload_pdf():
+    if not st.session_state.file_uploaded:
+        uploaded_file = st.file_uploader("Upload a PDF file", type="pdf")
+        return uploaded_file
+    return None
+
+def process_pdf(pdf_processor, uploaded_file):
+    st.session_state.file_uploaded = True
+    st.write("Processing the PDF...")
+    pdf_process = pdf_processor.process_pdf(uploaded_file)
+    if pdf_process == True:
+        st.session_state.pdf_processed = True
+        st.write("PDF processed successfully! You can now generate questions.")
+    else:
+        st.error("Failed to process the PDF.")
+
+def generate_quiz(pdf_processor):
+    if st.session_state.pdf_processed and st.button("Generate Quiz"):
+        st.write("Generating quiz based on the processed PDF...")
+        answer = pdf_processor.generate_questions()
+        st.write(answer)
 
 def main():
 
     load_api_keys()
-
+    pdf_processor = Pdf()
+    initialize_session_state()
     st.title("Vizuara Adaptive Q/A Generator")
-    st.subheader("Upload a PDF file to get started")
-
-    # File uploader for PDF and DOC files
-    uploaded_file = st.file_uploader("", type=["pdf"])
-    
-    if uploaded_file is not None:
-        st.success("File uploaded successfully!")
-        pp.process_pdf(uploaded_file)
-
-        query = st.text_input("Enter your query:")
-        if st.button("Submit Query"):
-            answer = pp.answer_query(query)
-            st.text_area("Result:", answer)
+    st.subheader("Answer adaptive questions based on your uploaded PDF!")
+    uploaded_file = upload_pdf()
+    if st.button("Generate Quiz"):
+        if uploaded_file:
+            st.write("generating quiz")
+            answer = pdf_processor.process_pdf(uploaded_file)
+            st.write(answer)
+        else:
+            st.error("Please upload a PDF file before processing.")
 
 
 
--- pdf_process.py
+++ pdf_process.py
@@ -1,89 +1,108 @@
 import os
+from langchain_community.document_loaders import PyPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+import pinecone
+from langchain_community.vectorstores import Pinecone
+from langchain_openai import OpenAIEmbeddings
+from pinecone import PodSpec
+from pinecone import ServerlessSpec
+from langchain.chains import RetrievalQA
+from langchain.chat_models import ChatOpenAI
+from dotenv import find_dotenv, load_dotenv
+      
+class Pdf():
 
-vector_store = None
+  pc = None
+  vector_store = None
 
-def process_pdf(uploaded_file):
-    pdf_data = load_pdf_document(uploaded_file)
-    pdf_chunks = chunk_pdf_data(pdf_data)
-    delete_pinecone_index()
+  def __init__(self) -> None:
+      self.vector_store = None
+      self.pc = pinecone.Pinecone()
 
-    global vector_store 
-    vector_store = insert_or_fetch_embeddings(index_name="photosynthesis", chunks=pdf_chunks)
-
-
-def answer_query(query):
-  from langchain.chains import RetrievalQA
-  from langchain.chat_models import ChatOpenAI
-
-  llm = ChatOpenAI(temperature=1, model_name="gpt-3.5-turbo")
-  retriever = vector_store.as_retriever(search_type='similarity', search_kwargs={'k': 3})
-  chain = RetrievalQA.from_chain_type(llm=llm, chain_type='stuff', retriever=retriever)
-  answer = chain.run(query)
-  return answer
-
-def insert_or_fetch_embeddings(index_name, chunks):
-  import pinecone
-  from langchain_community.vectorstores import Pinecone
-  from langchain_openai import OpenAIEmbeddings
-  from pinecone import PodSpec
-  from pinecone import ServerlessSpec
-
-  pc = pinecone.Pinecone()
-  embeddings = OpenAIEmbeddings(model = 'text-embedding-3-small', dimensions=1536)
-
-  if index_name in pc.list_indexes().names():
-    print(f'Index name: {index_name} already exists, loading embeddings')
-    vector_store = Pinecone.from_existing_index(index_name,embeddings)
-    print("fetched index")
-  else:
-    print(f"Creating Index: {index_name} and embeddings")
-    #pc.create_index(name=index_name, dimension=1536, metric='cosine', spec = PodSpec(environment='gcp-starter'))
-    pc.create_index(
-       name = index_name,
-       dimension = 1536,
-       metric = 'cosine',
-       spec = ServerlessSpec(
-          cloud = 'aws',
-          region = 'us-east-1'
-       )
-    )
-    vector_store = Pinecone.from_documents(chunks, embeddings, index_name=index_name)
-    print("created index")
-
-  return vector_store
-
-def delete_pinecone_index(index_name='all'):
-  import pinecone
-  pc = pinecone.Pinecone()
-  if index_name == 'all':
-    indexes = pc.list_indexes().names()
-    print("Deleting all indices")
-    for index in indexes:
-      pc.delete_index(index)
-
-  else:
-    pc.delete_index(index_name)
-
-def chunk_pdf_data(pdf_data):
-    from langchain.text_splitter import RecursiveCharacterTextSplitter
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size = 1024, chunk_overlap = 0)
-    chunks = text_splitter.split_documents(pdf_data)
-    return chunks
-
-def load_pdf_document(uploaded_file):
-    # Check if the uploaded file is a PDF
+  def load_pdf_document(self, uploaded_file):
     if uploaded_file.type == "application/pdf":
-        from langchain.document_loaders import PyPDFLoader
-        
         # Save the uploaded file to a temporary location
         with open(uploaded_file.name, "wb") as f:
             f.write(uploaded_file.getbuffer())
-        
-        # Load the PDF file
+    
         loader = PyPDFLoader(uploaded_file.name)
         data = loader.load()
-
-        # Remove the temporary file after loading
         os.remove(uploaded_file.name)
-        
         return data
+    
+  def chunk_pdf_data(self, pdf_data):
+      text_splitter = RecursiveCharacterTextSplitter(chunk_size = 1024, chunk_overlap = 0)
+      chunks = text_splitter.split_documents(pdf_data)
+      return chunks
+
+  def delete_pinecone_index(self, index_name='all'):
+    if index_name == 'all':
+      indexes = self.pc.list_indexes().names()
+      print("Deleting all indices")
+      for index in indexes:
+        self.pc.delete_index(index)
+
+    else:
+      self.pc.delete_index(index_name)
+
+  def insert_or_fetch_embeddings(self, index_name, chunks):
+      embeddings = OpenAIEmbeddings(model = 'text-embedding-3-small', dimensions=1536)
+
+      if index_name in self.pc.list_indexes().names():
+        print(f'Index name: {index_name} already exists, loading embeddings')
+        self.vector_store = Pinecone.from_existing_index(index_name,embeddings)
+        print("fetched index")
+      else:
+        print(f"Creating Index: {index_name} and embeddings")
+        #pc.create_index(name=index_name, dimension=1536, metric='cosine', spec = PodSpec(environment='gcp-starter'))
+        self.pc.create_index(
+          name = index_name,
+          dimension = 1536,
+          metric = 'cosine',
+          spec = ServerlessSpec(
+              cloud = 'aws',
+              region = 'us-east-1'
+          )
+        )
+        self.vector_store = Pinecone.from_documents(chunks, embeddings, index_name=index_name)
+        print("created index")
+
+  def generate_questions(self, query = "Generate 5 MCQS based on the content of this PDF."):
+      if self.vector_store is None:
+            raise ValueError("Vector store is not initialized.")
+      llm = ChatOpenAI(temperature=1, model_name="gpt-3.5-turbo")
+      retriever = self.vector_store.as_retriever(search_type='similarity', search_kwargs={'k': 3})
+      chain = RetrievalQA.from_chain_type(llm=llm, chain_type='stuff', retriever=retriever)
+      answer = chain.run(query)
+      print(f"Answer: {answer}")
+      return answer
+
+  def process_pdf(self, uploaded_file):
+      pdf_data = self.load_pdf_document(uploaded_file)
+      pdf_chunks = self.chunk_pdf_data(pdf_data)
+      print("chunk done")
+      self.delete_pinecone_index()
+      self.insert_or_fetch_embeddings(index_name="photosynthesis", chunks=pdf_chunks)
+      answer = self.generate_questions()
+      return answer
+
+  
+
+
+def load_api_keys():
+    load_dotenv(find_dotenv(".env.txt"))
+
+if __name__ == "__main__":
+   load_api_keys()
+   pdf = Pdf()
+   pdf.process_pdf()
+   pdf.generate_questions()
+
+
+
+
+
+
+
+
+
