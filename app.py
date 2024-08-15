f1af20ef21dbe32a7f548ee96f638a5b4e6977d1
Anusha Kalbande <anushakalbande17@gmail.com>
2024-08-15 15:09:11 -0400

    Consecutive questions

--- app.py
+++ app.py
@@ -14,16 +14,14 @@ def initialize_session_state():
         st.session_state.file_uploaded = False
     if 'quiz_started' not in st.session_state:
         st.session_state.quiz_started = None
-    if 'selected_option' not in st.session_state:
-        st.session_state.selected_option = None
     if 'question' not in st.session_state:
         st.session_state.question = None
     if 'options' not in st.session_state:
         st.session_state.options = []
     if 'correct_answer' not in st.session_state:
         st.session_state.correct_answer = None
-    # if 'answer_feedback' not in st.session_state:
-    #     st.session_state.answer_feedback = None
+    if 'answer_feedback' not in st.session_state:
+        st.session_state.answer_feedback = None
     # # Initialize session state for quiz status
     
     # if 'display_answer' not in st.session_state:
@@ -34,12 +32,12 @@ def reset_session_state():
     st.session_state.pdf_processor = None
     st.session_state.file_uploaded = False
     st.session_state.quiz_started = None
+    reset_question_state()
+   
+def reset_question_state():
     st.session_state.question = None
     st.session_state.options = []
     st.session_state.correct_answer = None
-    # st.session_state.answer_feedback = None
-   
-    # st.session_state.display_answer = False
 
 
 def display_title():
@@ -51,10 +49,22 @@ def get_question_from_pdf():
         st.session_state.question = question
         st.session_state.options = options
         st.session_state.correct_answer = correct_answer
-        st.session_state.selected_option = None
 
-
-
+def display_question():
+    st.markdown(f"**{st.session_state.question}**")
+    form = st.form(key = "quiz")
+    user_choice = form.radio("Choose an answer:", st.session_state.options, index=None)
+    submitted = form.form_submit_button("Submit your answer")
+    if submitted:
+        if user_choice == st.session_state.correct_answer:
+            st.success("Correct! Well done")
+            st.session_state.answer_feedback = True
+        else:
+            st.error("Incorrect!")
+            st.markdown(f"You selected: {user_choice}\n  Correct Answer : **{st.session_state.correct_answer}**")
+            st.session_state.answer_feedback = False
+        reset_question_state()
+        
 def main():
 
     load_api_keys()
@@ -83,16 +93,13 @@ def main():
         if st.session_state.question == None:
             get_question_from_pdf()
 
-        st.markdown(f"**{st.session_state.question}**")
-        form = st.form(key = "quiz")
-        user_choice = form.radio("Choose an answer:", st.session_state.options, index=None)
-        submitted = form.form_submit_button("Submit your answer")
-        if submitted:
-            if user_choice == st.session_state.correct_answer:
-                st.success("Correct! Well done")
-            else:
-                st.error("Incorrect!")
-                st.markdown(f"You selected: {user_choice}\n,  Correct Answer : **{st.session_state.correct_answer}**")
+        display_question()
+        
+        if st.session_state.answer_feedback is not None:
+            if st.button("Next"):
+                st.write("next")
+                #reset_question_state()
+                # get_question_from_pdf()
     
     elif st.session_state.quiz_started == False:
         st.subheader("Quiz stopped!")
